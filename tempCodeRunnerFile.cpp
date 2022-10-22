42c8b4027781e4f7fc11136239b18429696a134e
CodingReaper <asahoo381@gmail.com>
2022-10-23 03:10:54 +0530

    Indentation

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -3,26 +3,26 @@ using namespace std;
 class A
 {
 public:
-double add(double x, double y)
-{
-return x+y;
-}
-int add(int x, int y)
-{
-return x+y;
-}
-int add(int x, int y, int z)
-{
-return x+y+z;
-}
+    double add(double x, double y)
+    {
+        return x + y;
+    }
+    int add(int x, int y)
+    {
+        return x + y;
+    }
+    int add(int x, int y, int z)
+    {
+        return x + y + z;
+    }
 };
 
-//progam cont...
+// progam cont...
 int main()
 {
-A a;
-cout<<a.add(1, 2)<<endl;
-cout<<a.add(1.5, 2.5)<<endl;
-cout<<a.add(1, 2, 3)<<endl;
-return 0;
+    A a;
+    cout << a.add(1, 2) << endl;
+    cout << a.add(1.5, 2.5) << endl;
+    cout << a.add(1, 2, 3) << endl;
+    return 0;
 }
